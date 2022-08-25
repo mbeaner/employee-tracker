035bb161c997e866358c0ccb1eef74eea2523074
Mbeaner <email@example.com>
2022-08-25 10:31:54 -0400

    Updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -13,16 +13,16 @@ CREATE TABLE role (
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    CONSTRAINT FK_depart FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manger_id INT NULL,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manger_id) REFERENCES employee(id)
+    manager_id INT,
+    CONSTRAINT FK_roles FOREIGN KEY (role_id) REFERENCES role(id),
+    CONSTRAINT FK_manager FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
