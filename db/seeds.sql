d8d42d8c91fd5e744a427186dca0b74e1239f1ae
Mbeaner <email@example.com>
2022-08-25 10:31:14 -0400

    Updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,7 +14,7 @@ VALUES ("Sales Lead", 100000, 1),
         ("Legal Team Lead", 250000, 4),
         ("Lawyer", 190000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manger_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("John", "Doe", 1, null),
         ("Mike", "Chan", 2, 1),
         ("Ashley", "Rodriguez", 3, null),
